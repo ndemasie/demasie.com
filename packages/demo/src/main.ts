f1ee9dc21bf91cb6ae7684d5c86b3d199b048078
ndemasie <nathan.demasie@gmail.com>
2025-03-09 19:57:20 -0400

    some fire

--- packages/demo/src/main.ts
+++ packages/demo/src/main.ts
@@ -1,10 +1,10 @@
-import { Sandstorm } from 'objects/Sandstorm'
+import { Fire } from 'objects/Fire'
 import { Starfield } from 'objects/Starfield'
 
 import { App } from './App'
 
 App.instance.addObject(new Starfield())
-App.instance.addObject(new Sandstorm())
+App.instance.addObject(new Fire())
 
 document.body.appendChild(App.instance.renderer.domElement)
 window.addEventListener('resize', App.instance.resize, false)
