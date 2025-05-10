b83dea70a800fe666b92ebef62dfc7ab4439f7bb
ndemasie <nathan.demasie@gmail.com>
2025-05-09 23:39:32 -0400

    chore: use health GET

--- packages/nathan-app-referral-codes/src/pages/health.ts
+++ packages/nathan-app-referral-codes/src/pages/health.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from 'astro'
 
-export const HEAD: APIRoute = async () => {
+export const GET: APIRoute = async () => {
   return new Response('OK', {
     status: 200,
     headers: { 'Cache-Control': 'no-store' },
