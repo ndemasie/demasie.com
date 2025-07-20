dbdd4ba5ee1e3e2e2d9caed51ee63142e9fa7d51
ndemasie <nathan.demasie@gmail.com>
2025-07-20 14:39:13 -0400

    feat(nathan-app-refer): Add coinbase

--- packages/nathan-app-refer-codes/src/data/codes.ts
+++ packages/nathan-app-refer-codes/src/data/codes.ts
@@ -7,16 +7,17 @@ export type Brand = {
 
 //prettier-ignore
 export const brands: Brand[] = [
-  { key: 'airbnb', name: 'Airbnb', logoUrl: '/brands/airbnb_logo.svg' },
-  { key: 'chase', name: 'Chase', logoUrl: '/brands/chase_logo.svg', theme: 'light' },
-  { key: 'doordash', name: 'DoorDash', logoUrl: '/brands/doordash_logo.svg' },
-  { key: 'lyft', name: 'Lyft', logoUrl: '/brands/lyft_logo.svg' },
-  { key: 'mint-mobile', name: 'Mint Mobile', logoUrl: '/brands/mint_mobile.svg' },
-  { key: 'prenuvo', name: 'Prenuvo', logoUrl: '/brands/prenuvo_logo.svg', theme: 'light' },
-  { key: 'private-internet-access', name: 'Private Internet Access', logoUrl: '/brands/pia_logo.svg', theme: 'light'},
-  { key: 'purple', name: 'Purple', logoUrl: '/brands/purple_logo.svg' },
-  { key: 'uber', name: 'Uber', logoUrl: '/brands/uber_logo.svg', theme: 'light' },
-  { key: 'wise', name: 'Wise', logoUrl: '/brands/wise_logo.svg' },
+  { key: 'airbnb', name: 'Airbnb', logoUrl: '/brands/airbnb_wordmark.svg' },
+  { key: 'chase', name: 'Chase', logoUrl: '/brands/chase_wordmark.svg', theme: 'light' },
+  { key: 'coinbase', name: 'coinbase', logoUrl: '/brands/coinbase_wordmark.svg' },
+  { key: 'doordash', name: 'DoorDash', logoUrl: '/brands/doordash_wordmark.svg' },
+  { key: 'lyft', name: 'Lyft', logoUrl: '/brands/lyft_wordmark.svg' },
+  { key: 'mint-mobile', name: 'Mint Mobile', logoUrl: '/brands/mint_mobile_wordmark.svg' },
+  { key: 'prenuvo', name: 'Prenuvo', logoUrl: '/brands/prenuvo_wordmark.svg', theme: 'light' },
+  { key: 'private-internet-access', name: 'Private Internet Access', logoUrl: '/brands/pia_wordmark.svg', theme: 'light'},
+  { key: 'purple', name: 'Purple', logoUrl: '/brands/purple_wordmark.svg' },
+  { key: 'uber', name: 'Uber', logoUrl: '/brands/uber_wordmark.svg', theme: 'light' },
+  { key: 'wise', name: 'Wise', logoUrl: '/brands/wise_wordmark.svg' },
 ]
 
 export type Code = {
@@ -31,6 +32,7 @@ export const codes: Code[] = [
   { key: 'chase-bank', brandKey: 'chase', description: '$300 when opening a qualifying Chase checking account', url: 'https://accounts.chase.com/raf/share/2520037318?jp_aid_a=T_86282&jp_aid_p=private_conversation_deck%2fconversationDeckMessageThree' },
   { key: 'chase-freedom', brandKey: 'chase', description: '$200 bonus with the Chase Freedom Unlimited card', url: 'https://www.referyourchasecard.com/18A/4V5FN49WD5' },
   { key: 'chase-sapphire', brandKey: 'chase', description: 'Earn 100,000 bonus points with Chase Sapphire Preferred.', url: 'https://www.referyourchasecard.com/19r/KL37LHY2VX' },
+  { key: 'coinbase', brandKey: 'coinbase', description: '$30 for you after 1st trade of $20', url: 'https://coinbase.com/join/S2ZV6EL' },
   { key: 'doordash', brandKey: 'doordash', description: '$10 off your first 3 orders', url: 'https://drd.sh/nc4IL2bWVWYqs17p' },
   { key: 'lyft', brandKey: 'lyft', description: '50% off 2 rides', url: 'https://www.lyft.com/i/NATHAN67481?utm_medium=p2pi_iacc' },
   { key: 'mint-mobile', brandKey: 'mint-mobile', description: 'Get $15 in renewal credit', url: 'http://fbuy.me/vaRFL' },
